64f2ca64512775be84cf740b8f7ee5ba72774c71
Maxim Benyuh <max.bobo3@bk.ru>
2023-08-21 18:29:16 +0300

    feat: bubble-sort

--- problems/bubble-sort/bubble-sort.js
+++ problems/bubble-sort/bubble-sort.js
@@ -1,7 +1,18 @@
 // https://sortvisualizer.com/bubblesort/
 // https://en.wikipedia.org/wiki/Bubble_sort
 
-export function bubbleSort(arr) {
+export function bubbleSort(arr)
+{
+    for (let j = 0; j < arr.length; j++)
+        for (let i = 1; i < arr.length - j; i++)
+        {
+            if (arr[i - 1] > arr[i])
+            {
+                let tmp = arr[i - 1]
+                arr[i - 1] = arr[i];
+                arr[i] = tmp;
+            }
+        }
 
     return arr;
 }
