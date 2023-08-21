f56105a99d5b45d08f284766b5c6acc0da4a4d51
Maxim Benyuh <max.bobo3@bk.ru>
2023-08-21 19:55:26 +0300

    feat: valid-parenthesis

--- problems/valid-parenthesis/valid-parenthesis.js
+++ problems/valid-parenthesis/valid-parenthesis.js
@@ -1,5 +1,31 @@
 // https://leetcode.com/problems/valid-parentheses/
 
-export function isValid(str) {
+export function isValid(str)
+{
+    let stack = [];
+    let el;
+    let obj = {
+        "(": ")",
+        "{": "}",
+        "[": "]",
+    }
+    for (let i = 0; i < str.length; i++)
+    {
+        el = str[i]
+        if (el === "(" || el === "{" || el === "[")
+        {
+            stack.push(el)
+        } else if (el === ")" || el === "}" || el === "]")
+        {
+            if (obj[stack.at(-1)] === el)
+            {
+                stack.pop();
+            } else
+            {
+                return false
+            }
+        }
+    }
 
+    return stack.length === 0
 }
