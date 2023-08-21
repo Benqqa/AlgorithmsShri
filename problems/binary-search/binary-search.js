e054e4cd62f7551f5355c614e83635af8bc55afd
Maxim Benyuh <max.bobo3@bk.ru>
2023-08-21 20:44:26 +0300

    feat: binary-search

--- problems/binary-search/binary-search.js
+++ problems/binary-search/binary-search.js
@@ -1,5 +1,24 @@
 // https://en.wikipedia.org/wiki/Binary_search_algorithm
 
-export function binarySearch(arr, value) {
+export function binarySearch(arr, value)
+{
+    let leftValue = 0;
+    let rightValue = arr.length - 1;
+    let middleValue;
+    while (leftValue <= rightValue)
+    {
+        middleValue = Math.floor((rightValue + leftValue) / 2)
+        if (value < arr[middleValue])
+        {
+            rightValue = middleValue - 1;
+        } else if (value > arr[middleValue])
+        {
+            leftValue = middleValue + 1;
+        } else
+        {
+            return middleValue
+        }
+    }
 
+    return undefined
 }
