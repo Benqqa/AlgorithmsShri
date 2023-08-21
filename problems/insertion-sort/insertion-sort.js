583c54969e6e7b6f9c7e9dabbdf8497874475819
Maxim Benyuh <max.bobo3@bk.ru>
2023-08-21 19:05:15 +0300

    feat: insertion-sort

--- problems/insertion-sort/insertion-sort.js
+++ problems/insertion-sort/insertion-sort.js
@@ -1,7 +1,23 @@
 // https://sortvisualizer.com/insertionsort/
 // https://en.wikipedia.org/wiki/Insertion_sort
 
-export function insertionSort(arr) {
+export function insertionSort(arr)
+{
+    for (let j = 1; j < arr.length; j++)
+    {
+        for (let i = j; i > 0; i--)
+        {
+            if (arr[i] < arr[i - 1])
+            {
+                let tmp = arr[i]
+                arr[i] = arr[i - 1]
+                arr[i - 1] = tmp
+            } else
+            {
+                break;
+            }
+        }
+    }
 
     return arr;
 }
