eb745e3ad0f2f606c802341cd4ab0b78429b9e77
Maxim Benyuh <max.bobo3@bk.ru>
2023-08-21 20:25:23 +0300

    feat: prime-numbers

--- problems/prime-numbers/prime-numbers.js
+++ problems/prime-numbers/prime-numbers.js
@@ -1,7 +1,22 @@
 // https://ru.wikipedia.org/wiki/Решето_Эратосфена
 
-export function primeNumbers(num) {
+export function primeNumbers(num)
+{
     const result = [];
+    let arr = Array.from(Array(num), (_, index) => index + 1);
+    for (let j = 1; j < arr.length; j++)
+    {
+        let el = arr[j];
+        if (el !== -1)
+        {
+            result.push(el);
+            for (let i = j + el; i < arr.length; i += el)
+            {
+                arr[i] = -1
+            }
+        }
+
+    }
 
     return result;
 }
