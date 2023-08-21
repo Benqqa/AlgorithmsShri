fc68b91578316e6853c74fcb089de9b8bdb2c839
Maxim Benyuh <max.bobo3@bk.ru>
2023-08-21 21:55:41 +0300

    feat: maximum-subarray

--- problems/maximum-subarray/maximum-subarray.js
+++ problems/maximum-subarray/maximum-subarray.js
@@ -1,5 +1,22 @@
 // https://leetcode.com/problems/maximum-subarray/
 
-export function maximumSubarray(arr) {
+export function maximumSubarray(arr)
+{
+    let maxSum = -Infinity
+    for (let j = 0; j < arr.length; j++)
+    {
+        let left = 0;
+        while (left != j + 1)
+        {
+            let sum = arr[left];
+            for (let i = left + 1; i < j + 1; i++)
+            {
+                sum += arr[i];
+            }
+            maxSum = Math.max(sum, maxSum);
+            left++;
+        }
+    }
 
+    return maxSum;
 }
