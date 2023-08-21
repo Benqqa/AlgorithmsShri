46e46d8c34b2f135cddc5ba04dc617ac0476b7ad
Maxim Benyuh <max.bobo3@bk.ru>
2023-08-21 18:47:32 +0300

    feat: selection-sort

--- problems/selection-sort/selection-sort.js
+++ problems/selection-sort/selection-sort.js
@@ -1,7 +1,25 @@
 // https://sortvisualizer.com/selectionsort/
 // https://en.wikipedia.org/wiki/Selection_sort
 
-export function selectionSort(arr) {
+export function selectionSort(arr)
+{
+    for (let j = 0; j < arr.length; j++)
+    {
+        let min = arr[j];
+        let min_index = j
+        for (let i = j + 1; i < arr.length; i++)
+        {
+            if (arr[i] < min)
+            {
+                min = arr[i];
+                min_index = i
+            }
+        }
+        let tmp = arr[j]
+        arr[j] = arr[min_index]
+        arr[min_index] = tmp
+    }
+
 
     return arr;
 }
